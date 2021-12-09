ab018075d9c9f017470396cc13075e8baa39ee34
mairabassetto <mairabassetto@gmail.com>
2021-12-09 12:09:52 -0300

    Styling post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import Head from 'next/head';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -26,9 +29,61 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
+
+      <Header />
+      <img src="/teste.png" alt="teste" className={styles.banner} />
+
+      <main className={commonStyles.container}>
+        <section className={styles.post}>
+          <div className={styles.postTitle}>
+            <h1>Título</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                09 Dez 2021
+              </li>
+              <li>
+                <FiUser />
+                Maíra Bassetto
+              </li>
+              <li>
+                <FiClock />5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Título seção</h2>
+            <p>
+              Imagine que ao acessar um site que você goste muito pelo
+              smartphone você receba um aviso para adicionar o site à sua
+              homepage de aplicativos. Com o app instalado agora em seu celular,
+              você pode ter a mesma experiência que tinha pelo browser agora sem
+              nenhuma informação em tela além da aplicação, ou seja, toda
+              interface do navegador como barra de endereço, botões, favoritos,
+              etc, são removidos. <strong> Mas é igual um app nativo? </strong>
+              Não exatamente. Vamos discutir mais sobre isso durante
+              <a href="www.google.com"> exemplo de site</a> o post mas o PWA
+              difere em muitos aspectos de apps nativosque passam pelas lojas de
+              aplicativos como Google Play e App Store.
+            </p>
+            <p>
+              Ah, vale também lembrar que não é necessário escolher entre PWA ou
+              app nativo, inclusive grandes empresas possuem os dois, como
+              Twitter, Uber, Facebook, etc.
+            </p>
+          </article>
+        </section>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
