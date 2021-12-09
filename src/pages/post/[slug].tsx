f0da27ba696170f57547a33fec47341c442e3273
mairabassetto <mairabassetto@gmail.com>
2021-12-09 15:23:42 -0300

    Config post page slug

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,7 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import Head from 'next/head';
+import { RichText } from 'prismic-dom';
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { useRouter } from 'next/router';
 import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 
@@ -29,20 +32,26 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }: PostProps) {
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
+
   return (
     <>
       <Head>
-        <title>Home | SpaceTraveling</title>
+        <title>{post.data.title} | SpaceTraveling</title>
       </Head>
 
       <Header />
-      <img src="/teste.png" alt="teste" className={styles.banner} />
+      <img src={post.data.banner.url} alt="imagem" className={styles.banner} />
 
       <main className={commonStyles.container}>
         <section className={styles.post}>
           <div className={styles.postTitle}>
-            <h1>Título</h1>
+            <h1>{post.data.title}</h1>
             <ul>
               <li>
                 <FiCalendar />
@@ -50,7 +59,7 @@ export default function Post({ post }: PostProps) {
               </li>
               <li>
                 <FiUser />
-                Maíra Bassetto
+                {post.data.author}
               </li>
               <li>
                 <FiClock />5 min
@@ -58,43 +67,73 @@ export default function Post({ post }: PostProps) {
             </ul>
           </div>
 
-          <article>
-            <h2>Título seção</h2>
-            <p>
-              Imagine que ao acessar um site que você goste muito pelo
-              smartphone você receba um aviso para adicionar o site à sua
-              homepage de aplicativos. Com o app instalado agora em seu celular,
-              você pode ter a mesma experiência que tinha pelo browser agora sem
-              nenhuma informação em tela além da aplicação, ou seja, toda
-              interface do navegador como barra de endereço, botões, favoritos,
-              etc, são removidos. <strong> Mas é igual um app nativo? </strong>
-              Não exatamente. Vamos discutir mais sobre isso durante
-              <a href="www.google.com"> exemplo de site</a> o post mas o PWA
-              difere em muitos aspectos de apps nativosque passam pelas lojas de
-              aplicativos como Google Play e App Store.
-            </p>
-            <p>
-              Ah, vale também lembrar que não é necessário escolher entre PWA ou
-              app nativo, inclusive grandes empresas possuem os dois, como
-              Twitter, Uber, Facebook, etc.
-            </p>
-          </article>
+          {post.data.content.map(content => {
+            return (
+              <article key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div
+                  className={styles.postContent}
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </article>
+            );
+          })}
         </section>
       </main>
     </>
   );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
 
-//   // TODO
-// };
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body],
+        };
+      }),
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
