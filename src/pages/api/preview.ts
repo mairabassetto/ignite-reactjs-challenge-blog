ffe53b0ac3a105b1030ef2f56bb0d2fe6ae806d2
mairabassetto <mairabassetto@gmail.com>
2021-12-17 17:06:58 -0300

    Add preview Prismic

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,23 @@
+import { linkResolver } from '../../../prismicConfiguration';
+import { Client } from '../../utils/prismicHelpers.js';
+
+// eslint-disable-next-line consistent-return
+export default async (req, res) => {
+  const { token: ref, documentId } = req.query;
+  const redirectUrl = await Client(req)
+    .getPreviewResolver(ref, documentId)
+    .resolve(linkResolver, '/');
+
+  if (!redirectUrl) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  res.setPreviewData({ ref });
+
+  res.write(
+    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+    <script>window.location.href = '${redirectUrl}'</script>
+    </head>`
+  );
+  res.end();
+};
---
+++ src/utils/prismicHelpers.js
@@ -0,0 +1,39 @@
+import Prismic from '@prismicio/client';
+import Link from 'next/link';
+import {
+  apiEndpoint,
+  accessToken,
+  linkResolver,
+  Router,
+} from '../../prismicConfiguration';
+
+export const customLink = (type, element, content, children, index) => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <Link key={index} href={linkResolver(element.data)}>
+    <a>{content}</a>
+  </Link>
+);
+
+export const Client = (req = null) =>
+  // eslint-disable-next-line no-use-before-define
+  Prismic.client(apiEndpoint, createClientOptions(req, accessToken, Router)),
+
+// eslint-disable-next-line prettier/prettier
+const createClientOptions = (
+  req = null,
+  prismicAccessToken = null,
+  routes = null
+) => {
+  const reqOption = req ? { req } : {};
+  const accessTokenOption = prismicAccessToken
+    ? { accessToken: prismicAccessToken }
+    : {};
+  const routesOption = routes ? { routes: Router.routes } : {}
+  return {
+    ...reqOption,
+    ...accessTokenOption,
+    ...routesOption,
+  }
+}
+
+export default Client;
